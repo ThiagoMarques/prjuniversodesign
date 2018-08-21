a4636a95b1309ab1f1ad9923c63e97c47d1cda8f
Ivonilson <ivonilsoncardoso@hotmail.com>
2018-08-21 18:58:50 -0300

    atualização de layout

--- principal.php
+++ principal.php
@@ -4,37 +4,145 @@
 	<title>Principal</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="estilos/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="estilos/teste.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<!---área de input superior de Login para acesso a área restrita - 12 colunas no grid-->
-	<div class="col-md-12">
-		<label>Login</label><input type="text" name="iptEmail" placeholder="Email">
-		<label>Senha</label><input type="password" name="iptSenha" placeholder="Senha">
-		<button type="submit" class="btn btn-primary">Entrar</button>
-		<img src=""><label>Esqueci minha senha</label>
+	<!---área de input superior de Login para acesso a área restrita - 12 colunas no grid -->
+		<div class="container" align="right">
+			<div class="row-fluid">
+				<div class="col col-md-12 divTeste">
+				<label>Login</label><input type="text" name="iptEmail" placeholder="Email">
+				<label>Senha</label><input type="password" name="iptSenha" placeholder="Senha">
+				<button type="submit" class="btn btn-primary">Entrar</button>
+				<img src=""><label>Esqueci minha senha</label>
+			</div>
+		</div>
 	</div>
+	
+	<div class="container">
+		<div class="row-fluid">
+			<!--- área da logo - 02 colunas no grid -->
+			<div class="col col col-md-2 text-center divTeste">
+				LOGOTIPO
+			</div>
 
-	<!--- área da logo - 02 colunas no grid-->
-	<div>
-		
-	</div>
+			<!---área de input de buscas no site - 08 colunas no grid-->
+			<div class="col col-md-8 text-center divTeste">
+				INPUTS DE PESQUISA
+			</div>
 
-	<!---área de input de buscas no site - 08 colunas no grid-->
-	<div>
-		
+			<!--área de cadastro de clientes - 02 colunas no grid-->
+			<div class="col col-md-2 text-center divTeste">
+				CADASTRO DE CLIENTES
+			</div>
+		</div>
 	</div>
-
-	<!--área de cadastro de clientes - 02 colunas no grid-->
-	<div>
-		
-	</div>
+	
 
 	<!--área de navegação do site (menu) - 12 colunas no grid -->
+	<div class="container">
+		<div class="col col-md-12 container-fluid divTeste">
+			<ul>
+				<li class="col-md-1 divTeste">Item 1</li>
+				<li class="col-md-1 divTeste">Item 2</li>
+				<li class="col-md-1 divTeste">Item 3</li>
+				<li class="col-md-1 divTeste">Item 4</li>
+				<li class="col-md-1 divTeste">Item 5</li>
+				<li class="col-md-1 divTeste">Item 6</li>
+				<li class="col-md-1 divTeste">Item 7</li>
+				<li class="col-md-1 divTeste">Item 8</li>
+				<li class="col-md-1 divTeste">Item 9</li>
+				<li class="col-md-1 divTeste">Item 10</li>
+				<li class="col-md-1 divTeste">Item 11</li>
+				<li class="col-md-1 divTeste">Item 12</li>
+			</ul>
+		</div>
+		
+	</div>
+
+	<!-- slider princpal - 12 colunas no grid-->
+	<div class="container">
+		<div class="row-fluid divTeste text-center" style="height: 300px">
+			<div class="col col-md-12">
+				<br>
+				<br>
+				<br>
+				<br>
+				SLIDER
+			</div>
+		</div>
+	</div>
+
+	<!--- menu lateral - 02 colunas no grid -->
+	<div class="container">
+		<div class="row-fluid">
+			<div>
+				<ul class="col-md-2 divTeste text-center">
+				<li>Item 1</li>
+				<li>Item 2</li>
+				<li>Item 3</li>
+				<li>Item 4</li>
+				<li>Item 5</li>
+				<li>Item 6</li>
+				<li>Item 7</li>
+				<li>Item 8</li>
+				<li>Item 9</li>
+				<li>Item 10</li>
+				<li>Item 11</li>
+				<li>Item 12</li>
+				<li>Item 13</li>
+				<li>Item 14</li>
+				<li>Item 15</li>
+				<li>Item 16</li>
+				<li>Item 17</li>
+				<li>Item 18</li>
+				<li>Item 19</li>
+				<li>Item 20</li>
+				<li>Item 21</li>
+				<li>Item 22</li>
+				<li>Item 23</li>
+				<li>Item 24</li>
+				<li>Item 25</li>
+				<li>Item 26</li>
+				<li>Item 27</li>
+				<li>Item 28</li>
+				<li>Item 29</li>
+				<li>Item 30</li>
+			</ul>
+			</div>
+		</div>
+	</div>
+
+	<!-- área de imagens com conteúdo principal do site - 10 colunas no grid -->
 	<div>
 		
 	</div>
-	teste
+
+	<!-- área de newsletter - 12 colunas no grid -->
+	<div>
+		
+	</div>
+
+	<!--- área de contatos telefonicos - 04 colunas no grid -->
+	<div>
+		
+	</div>
+
+	<!-- área de contatos via rede social - 04 colunas no grid -->
+	<div>
+		
+	</div>
+
+	<!-- área de contatos diversos e promocional - 04 colunas no grid -->
+	<div>
+		
+	</div>
+
+	<!-- rodapé - 12 colunas no grid -->
+	<div>
+		
+	</div>
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/jquery/1.11.3/jquery.min.js"></script>
 <script type="text/javascript" src="estilos/bootstrap/js/bootstrap.min.js"></script>
