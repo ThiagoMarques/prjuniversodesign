4ed4f236d91fce3975d8d336c8edcef69b0b9230
Ivonilson <ivonilsoncardoso@hotmail.com>
2018-08-21 15:20:01 -0300

    inclusão de divs

--- principal.php
+++ principal.php
@@ -7,11 +7,32 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+	<!---área de input superior de Login para acesso a área restrita - 12 colunas no grid-->
 	<div class="col-md-12">
 		<label>Login</label><input type="text" name="iptEmail" placeholder="Email">
 		<label>Senha</label><input type="password" name="iptSenha" placeholder="Senha">
 		<button type="submit" class="btn btn-primary">Entrar</button>
 		<img src=""><label>Esqueci minha senha</label>
+	</div>
+
+	<!--- área da logo - 02 colunas no grid-->
+	<div>
+		
+	</div>
+
+	<!---área de input de buscas no site - 08 colunas no grid-->
+	<div>
+		
+	</div>
+
+	<!--área de cadastro de clientes - 02 colunas no grid-->
+	<div>
+		
+	</div>
+
+	<!--área de navegação do site (menu) - 12 colunas no grid -->
+	<div>
+		
 	</div>
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/jquery/1.11.3/jquery.min.js"></script>
