63216ea78036791cde3d464c2db879c111d68d59
IVONILSON <ivonilsoncardoso@hotmail.com>
2018-08-21 11:06:46 -0300

    primeiro envio - projeto

---
+++ principal.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<htm lang="pt-br">
+<head>
+	<title>Principal</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilos/bootstrap/css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div class="col-md-12">
+		<label>Login</label><input type="text" name="iptEmail" placeholder="Email">
+		<label>Senha</label><input type="password" name="iptSenha" placeholder="Senha">
+		<button type="submit" class="btn btn-primary">Entrar</button>
+		<img src=""><label>Esqueci minha senha</label>
+	</div>
+
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="estilos/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
